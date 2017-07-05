d418c1021bc500961a0e84d0cbc8dfec902c3448
Rohan <rohangho@live.com>
2017-07-06 01:49:30 +0530

    every thing running correctly

---
+++ 4/Bookview/app/src/main/java/com/example/android/bookview/Books.java
@@ -0,0 +1,52 @@
+package com.example.android.bookview;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books extends AppCompatActivity {
+
+
+    private static final String USGS_REQUEST_URL ="https://www.googleapis.com/books/v1/volumes?q=harry";
+    private adapter mAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+        ListView listView = (ListView) findViewById(R.id.list);
+        mAdapter = new adapter(this, new ArrayList<custom>());
+        listView.setAdapter(mAdapter);
+        bookAsyncTask task = new bookAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+    private class bookAsyncTask extends AsyncTask<String, Void, List<custom>> {
+        @Override
+        protected List<custom> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            List<custom> result = QueryUtils.fetchData(urls[0]);
+            return result;
+        }
+        @Override
+        protected void onPostExecute(List<custom> data) {
+            mAdapter.clear();
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+        }
+    }
+}
+
+
